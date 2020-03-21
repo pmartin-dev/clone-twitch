6aa0cc7541bcd18e6088c497ddc0457c5e2f4f2e
pmartin-dev <pm4rtin@gmail.com>
2020-03-21 11:26:39 +0100

    Project done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Games from "./components/Games/Games";
+import TopStreams from "./components/TopStreams/TopStreams";
+import GameStreams from "./components/GameStreams/GameStreams"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Live from "./components/Live/Live"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Sidebar />
+        <Switch>
+          <Route exact path="/" component={Games}/>
+          <Route exact path="/top-streams" component={TopStreams}/>
+          <Route exact path="/live/:slug" component={Live}/>
+          <Route exact path="/game/:slug" component={GameStreams}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
